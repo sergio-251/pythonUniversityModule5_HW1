3033a9ec26a4e7255ee1295905d63349e57e570a
sergio-251 <50610151+sergio-251@users.noreply.github.com>
2024-11-15 13:02:49 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,17 @@
+# Атрибуты и методы объекта
+
+class House:
+    def __init__(self, name, number_of_floors: int):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        if new_floor <= self.number_of_floors:
+            print(*[i for i in range(1, new_floor + 1)], sep='\n')
+        else:
+            print('Такого этажа не существует')
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
